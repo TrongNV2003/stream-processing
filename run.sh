d80d27e10fb0648ee4f66b269bca0efb1ab1882a
trongnv2003 <trongnv.bi12-449@st.usth.edu.vn>
2025-04-23 00:10:31 +0700

    refactor: read data in chunks

--- run.sh
+++ run.sh
@@ -1,5 +1,1 @@
-python -m stream_processing.app \
-    --processing_mode micro_batch \
-    --batch_size 8 \
-    --batch_timeout 0.1 \
-    --use_torch \
+python -m stream_processing.app 
--- stream_processing/api.py
+++ stream_processing/api.py
@@ -13,26 +13,50 @@ app_logger = AppLogger.get_instance()
 def get_storage_service():
     return DatabaseHandler()
 
-async def ingest_in_background(content: bytes):
+async def ingest_in_background(chunks: list[bytes]):
     producer_service = KafkaProducerService()
-    await producer_service.ingest_data(kafka_config.raw_data_topic, content)
+    for chunk in chunks:
+        await producer_service.ingest_data(kafka_config.raw_data_topic, chunk)
 
 @app.post("/ingest")
 async def ingest(background_tasks: BackgroundTasks):
-    with open("stream_processing/data_test_stream/test_dataset.csv", "rb") as file:
-        content = file.read()
-        
-    background_tasks.add_task(ingest_in_background, content) # Chạy trong background
-    return {"status": "success", "message": "Record network traffic to Kafka"}
+    chunk_size = 1024 * 1024
+    chunks = []
+    try:
+        with open("stream_processing/data_test_stream/dataset.csv", "rb") as file:
+            while True:
+                chunk = file.read(chunk_size)
+                if not chunk:
+                    break
+                chunks.append(chunk)
+            
+        background_tasks.add_task(ingest_in_background, chunks) # Chạy trong background
+        return {"status": "success", "message": "Record network traffic to Kafka"}
+    except Exception as e:
+        app_logger.log(f"Ingest error: {str(e)}", level="error")
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/ingest/upload")
 async def ingest_upload(file: UploadFile = File(...), background_tasks: BackgroundTasks = BackgroundTasks()):
     if not file.filename.endswith(".csv"):
         raise HTTPException(status_code=400, detail="Only csv files are supported")
     
-    content = await file.read()
-    background_tasks.add_task(ingest_in_background, content)
-    return {"status": "success", "message": "Record network traffic to Kafka"}
+    chunk_size = 1024 * 1024
+    chunks = []
+    
+    try:
+        while True:
+            chunk = await file.read(chunk_size)
+            if not chunk:
+                break
+            chunks.append(chunk)
+        
+        background_tasks.add_task(ingest_in_background, chunks)
+        return {"status": "success", "message": "Record network traffic to Kafka"}
+    
+    except Exception as e:
+        app_logger.log(f"Ingest upload error: {str(e)}", level="error")
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/results")
 def get_results(limit: int = 10, service: DatabaseHandler = Depends(get_storage_service)):
